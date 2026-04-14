a49c8e5116f6e05ad3af89c9e7202d0978614e28
Hemant deora <hemantdeora923@gmail.com>
2026-04-14 15:48:14 +0530

    small changes

--- HospitalManagementSystem/src/main/java/com/training/Repo/PatientRepo.java
+++ HospitalManagementSystem/src/main/java/com/training/Repo/PatientRepo.java
@@ -2,6 +2,7 @@ package com.training.Repo;
 
 import com.training.Entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +10,6 @@ import java.util.List;
 @Repository
 public interface PatientRepo extends JpaRepository<Patient, Long> {
 
+    @Query("SELECT p FROM Patient p WHERE p.firstName = :firstName")
     public List<Patient> findByFirstName(String firstName);
 }
